e5758a5220e30d1ed7530e6f21d4c1061a6835b9
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-09-13 11:28:56 +0200

    azure pipeline

---
+++ src/main/java/it/gov/pagopa/Prova.java
@@ -0,0 +1,15 @@
+package it.gov.pagopa;
+
+
+public class Prova {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
+
+}
