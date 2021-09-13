d30547cedee2574942f145ead067bd988fef07d3
ddstkmuser1 <cons.ferrogiuseppe@sia.eu>
2021-09-13 11:49:29 +0200

    terraform update

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -2,6 +2,9 @@ resource "kubernetes_ingress" "ingress" {
   metadata {
     name      = "pm-cloud-mock-ingress-service"
     namespace = kubernetes_namespace.pmnamespace.metadata[0].name
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/$1"
+    }
   }
 
   spec {
