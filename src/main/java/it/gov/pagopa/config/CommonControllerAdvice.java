c0222ed6bc0d855050d35881926391cbc389f7ae
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-10-01 16:13:29 +0200

    PM-164 try to fix js and css download

--- src/main/java/it/gov/pagopa/config/CommonControllerAdvice.java
+++ src/main/java/it/gov/pagopa/config/CommonControllerAdvice.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.config;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class CommonControllerAdvice {
 
-    protected static final String APP_VERSION = "APP_VERSION";
+    private static final String APP_VERSION = "APP_VERSION";
+    private static final String AZURE_API_STATIC_RES = "AZURE_API_STATIC_RES";
     private final String appVersion;
+
+    @Value("${server.azure-api-static-res}")
+    private String azureApiStaticRes;
 
 
     public CommonControllerAdvice() {
@@ -22,6 +27,7 @@ public class CommonControllerAdvice {
     @ModelAttribute
     public void advice(ModelMap modelMap) {
         modelMap.addAttribute(APP_VERSION, appVersion);
+        modelMap.addAttribute(AZURE_API_STATIC_RES, StringUtils.removeEnd(azureApiStaticRes, "/"));
     }
 
 }
