83f5f5d39c2d702e679bb5fee9ac4f627d046506
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-10-11 17:22:54 +0200

    PM-202 refund: added new fields in the request

--- developenviroment/sql_versions/V4__PM_202_MANAGE_MORE_CLIENTS.sql
+++ developenviroment/sql_versions/V4__PM_202_MANAGE_MORE_CLIENTS.sql
@@ -3,22 +3,24 @@ delete from pp_onboarding_back;
 delete from user_paypal;
 delete from pp_paypal_management;
 
-ALTER TABLE pm_mock_local.pp_onboarding_back ADD client_id integer NOT NULL;
+ALTER TABLE pp_onboarding_back ADD client_id integer NOT NULL;
 ALTER TABLE pp_onboarding_back ADD CONSTRAINT fk_pob_client FOREIGN KEY(client_id) REFERENCES client(id);
 
-ALTER TABLE pm_mock_local.user_paypal ADD client_id integer NOT NULL;
+ALTER TABLE user_paypal ADD client_id integer NOT NULL;
 ALTER TABLE user_paypal ADD CONSTRAINT fk_up_client FOREIGN KEY(client_id) REFERENCES client(id);
 
-ALTER TABLE pm_mock_local.pp_paypal_management ADD client_id integer NOT NULL;
+ALTER TABLE pp_paypal_management ADD client_id integer NOT NULL;
 ALTER TABLE pp_paypal_management ADD CONSTRAINT fk_ppm_client FOREIGN KEY(client_id) REFERENCES client(id);
+
+DROP INDEX user_paypal_id_appio_contract_number_idx;
+DROP INDEX user_paypal_id_appio_idx;
+DROP INDEX pp_onboarding_back_id_appio_idx;
 
 CREATE UNIQUE INDEX ON user_paypal(id_appio, client_id)WHERE NOT DELETED;
 CREATE UNIQUE INDEX ON user_paypal(id_appio, client_id, contract_number)WHERE NOT DELETED;
+CREATE UNIQUE INDEX ON pp_onboarding_back(id_appio, client_id)WHERE NOT used
 
-DROP INDEX pm_mock_local.user_paypal_id_appio_contract_number_idx;
-DROP INDEX pm_mock_local.user_paypal_id_appio_idx;
+ALTER TABLE client ADD base_url varchar(256);
+ALTER TABLE client ADD CONSTRAINT client_un UNIQUE (client_name);
 
-ALTER TABLE pm_mock_local.client ADD base_url varchar(256);
-ALTER TABLE pm_mock_local.client ADD CONSTRAINT client_un UNIQUE (client_name);
-
-UPDATE pm_mock_local.client SET base_url='http://localhost:8080' WHERE client_name='local';
+UPDATE client SET base_url='http://localhost:8080' WHERE client_name='local';
--- src/main/java/it/gov/pagopa/paypalpsp/dto/PpRefundDirectRequest.java
+++ src/main/java/it/gov/pagopa/paypalpsp/dto/PpRefundDirectRequest.java
@@ -6,16 +6,24 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PpRefundDirectRequest {
+    @NotNull
+    @JsonProperty("id_appio")
+    private String idAppIo;
 
     @NotNull
     @JsonProperty("id_trs_appio")
     private String idTrsAppIo;
 
+    @NotNull
+    @Digits(fraction = 2, integer = Integer.MAX_VALUE)
+    private BigDecimal importo;
 }
