a40cd4708860befd9d8149b7319eade7f34f0d86
David Bertoni <david.bertoni@gft.com>
2022-03-24 10:23:51 +0100

    PM-411 - Adding endpoint to change outcome and timeout

--- src/main/java/it/gov/pagopa/bpay/controller/BPayClientController.java
+++ src/main/java/it/gov/pagopa/bpay/controller/BPayClientController.java
@@ -6,19 +6,38 @@ import lombok.extern.log4j.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.*;
+
 @Log4j2
 @RestController
-@RequestMapping("/bpayweb/client")
+@RequestMapping("/bpayweb/change")
 public class BPayClientController {
 
     @Autowired
     private TableConfigRepository configRepository;
 
-    @PostMapping("/change")
+    private TableConfig outcomeConfig;
+    private TableConfig timeoutConfig;
+
+    @PostConstruct
+    public void init() {
+        outcomeConfig = configRepository.findByPropertyKey("BPAY_PAYMENT_OUTCOME");
+        timeoutConfig = configRepository.findByPropertyKey("BPAY_PAYMENT_TIMEOUT");
+    }
+
+    @PostMapping("/client")
     public void changeClient(@RequestBody String client) {
         TableConfig currentClient = configRepository.findByPropertyKey("BPAY_CURRENT_CLIENT");
         currentClient.setPropertyValue(client);
         configRepository.save(currentClient);
+    }
+
+    @PostMapping("/outcome")
+    public void changeOutcome(@RequestParam(required = false) String code, @RequestParam(required = false) String timeout) {
+        outcomeConfig.setPropertyValue(code);
+        configRepository.save(outcomeConfig);
+        timeoutConfig.setPropertyValue(timeout != null ? timeout : "false");
+        configRepository.save(timeoutConfig);
     }
 
 }
