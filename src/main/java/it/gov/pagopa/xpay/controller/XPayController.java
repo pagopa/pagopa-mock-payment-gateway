13642076121c4d2e9566f4fbc1647d562bb21e84
Alessandro Dossi <alessandro.dossi@external.nexigroup.com>
2022-09-19 15:26:30 +0200

    Refactoring

--- src/main/java/it/gov/pagopa/xpay/controller/XPayController.java
+++ src/main/java/it/gov/pagopa/xpay/controller/XPayController.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.xpay.controller;
 
-import it.gov.pagopa.db.repository.TableConfigRepository;
 import it.gov.pagopa.xpay.dto.EsitoXpay;
 import it.gov.pagopa.xpay.dto.XPayAuthRequest;
 import it.gov.pagopa.xpay.dto.XPayAuthResponse;
@@ -8,6 +7,7 @@ import it.gov.pagopa.xpay.dto.XpayError;
 import it.gov.pagopa.xpay.entity.XPayPayment;
 import it.gov.pagopa.xpay.repository.XPayRepository;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -37,48 +37,47 @@ public class XPayController {
     @Autowired
     private XPayRepository xPayRepository;
 
-    private final String HTMLTORETURN = "<html>" +
+    private static final String HTML_TO_RETURN = "<html>" +
             "   <head>" +
             "      <title>Gestione Pagamento - Autenticazione</title>" +
-            "      <script type=\"text/javascript\" language=\"javascript\">function moveWindow() { document.tdsFraudForm.submit();}</script>" +
+            "      <script type=\"text/javascript\" language=\"javascript\">function moveWindow() { " +
+            "           document.tdsFraudForm.submit();}</script>" +
             "   </head>" +
             "   <body>" +
-            "      <form name=\"tdsFraudForm\" action=\"mock-psp-xpay-view\" method=\"POST\"><input type=\"hidden\" name=\"action\"     value=\"fraud\"><input type=\"hidden\" name=\"merchantId\" value=\"31320986\"><input type=\"hidden\" name=\"description\" value=\"7090132540_1663077273191\"><input type=\"hidden\" name=\"gdiUrl\"      value=\"\"><input type=\"hidden\" name=\"gdiNotify\"   value=\"\"></form>" +
+            "      <form name=\"tdsFraudForm\" action=\"mock-psp-xpay-view\" method=\"POST\"><input type=\"hidden\" " +
+            "       name=\"action\" value=\"fraud\"><input type=\"hidden\" name=\"merchantId\" value=\"31320986\">" +
+            "       <input type=\"hidden\" name=\"description\" value=\"7090132540_1663077273191\"><input type=\"hidden\"" +
+            "       name=\"gdiUrl\" value=\"\"><input type=\"hidden\" name=\"gdiNotify\" value=\"\"></form>" +
             "      <script type=\"text/javascript\"> moveWindow(); </script>" +
             "   </body>" +
             "</html>";
 
     @PostMapping("/ecomm/api/paga/autenticazione3DS")
     public ResponseEntity<Object> paymentAuthorization(@RequestBody XPayAuthRequest request) {
-
-        log.info("Create AuthResponse for transactioId: " + request.getCodiceTransazione());
-
-        Long timeStamp =System.currentTimeMillis();
-        String idOperazione =  UUID.randomUUID().toString();
-
+        String codiceTransazione = request.getCodiceTransazione();
+        log.info("XPay autenticazione3DS - create AuthResponse for transactionId: " + codiceTransazione);
+        Long timeStamp = System.currentTimeMillis();
+        String idOperazione = UUID.randomUUID().toString();
         try {
-            String macToReturn = getMacToReturn(request.getCodiceTransazione(), request.getDivisa(), request.getImporto(), request.getTimeStamp());
-
-            saveXpayOnDb(idOperazione, request, timeStamp);
-
-            if (request.getMac().equals(macToReturn)) {
+            saveXpayRequest(idOperazione, request, timeStamp);
+            String macToReturn = getMacToReturn(codiceTransazione, request.getDivisa(), request.getImporto(), request.getTimeStamp());
+            if (StringUtils.equals(request.getMac(), macToReturn)) {
+                log.info("MAC verified. Generating response");
                 return createOkResponse(timeStamp, macToReturn, idOperazione);
-
             } else {
+                log.info("MAC not verified. Generating KO response");
                 XpayError xpayError = new XpayError(3L, "MAC errato");
                 return createKoResponse(timeStamp, xpayError, macToReturn, idOperazione);
-
             }
         } catch (Exception e) {
+            log.error("An exception occurred while processing the request", e);
             XpayError xpayError = new XpayError(50L, "Impossibile calcolare il mac");
-            return createKoResponse(timeStamp, xpayError, null, idOperazione);
+            return createKoResponse(timeStamp, xpayError, "error", idOperazione);
         }
-
     }
 
-    private void saveXpayOnDb(String idOperazione, XPayAuthRequest request, Long timeStamp) {
+    private void saveXpayRequest(String idOperazione, XPayAuthRequest request, Long timeStamp) {
         XPayPayment xPayPayment = new XPayPayment();
-
         xPayPayment.setIdOperazione(idOperazione);
         xPayPayment.setApiKey(request.getApiKey());
         xPayPayment.setCvv(request.getCvv());
@@ -91,22 +90,17 @@ public class XPayController {
         xPayPayment.setTimeStamp_request(request.getTimeStamp());
         xPayPayment.setTimeStamp_response(timeStamp);
         xPayPayment.setUrlRisposta(request.getUrlRisposta());
-
-
         xPayRepository.save(xPayPayment);
-
     }
 
     private ResponseEntity<Object> createOkResponse(Long timeStamp, String mac, String idOperazione) {
         XPayAuthResponse xPayAuthResponse = new XPayAuthResponse();
         xPayAuthResponse.setEsito(EsitoXpay.OK);
         xPayAuthResponse.setIdOperazione(idOperazione);
-        xPayAuthResponse.setHtml(HTMLTORETURN);
+        xPayAuthResponse.setHtml(HTML_TO_RETURN);
         xPayAuthResponse.setTimeStamp(timeStamp);
         xPayAuthResponse.setMac(mac);
-
         return ResponseEntity.ok().body(xPayAuthResponse);
-
     }
 
     private ResponseEntity<Object> createKoResponse(Long timeStamp, XpayError xpayError, String mac, String idOperazione) {
@@ -116,27 +110,23 @@ public class XPayController {
         xPayAuthResponse.setTimeStamp(timeStamp);
         xPayAuthResponse.setMac(mac);
         xPayAuthResponse.setErrore(xpayError);
-
         return ResponseEntity.internalServerError().body(xPayAuthResponse);
     }
 
     private String getMacToReturn(String codiceTransazione, Long divisa, BigInteger importo, String timeStamp) throws Exception {
-        String realMac = String.format("apiKey=%scodiceTransazione=%sdivisa=%simporto=%stimeStamp=%s%s", apiKey, codiceTransazione, divisa, importo, timeStamp, chiaveSegreta);
-        
+        log.info("Generating MAC for transactionId: " + codiceTransazione);
+        String realMac = String.format("apiKey=%scodiceTransazione=%sdivisa=%simporto=%stimeStamp=%s%s",
+                apiKey, codiceTransazione, divisa, importo, timeStamp, chiaveSegreta);
         return hashMac(realMac);
-
     }
 
     private String hashMac(String realMac) throws Exception {
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
         byte[] in = digest.digest(realMac.getBytes(StandardCharsets.UTF_8));
-
         final StringBuilder builder = new StringBuilder();
         for (byte b : in) {
             builder.append(String.format("%02x", b));
         }
         return builder.toString();
     }
-
-
 }
